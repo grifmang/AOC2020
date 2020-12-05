ad283d005c49ce2aeb6f9b8a2a5dfd37d85c2301
Tim Griffith <grifmang@gmail.com>
2020-12-04 21:44:57 -0500

    Day Four Part Two complete

--- Day 4/dayfour2.py
+++ Day 4/dayfour2.py
@@ -30,60 +30,117 @@ def clean_data():
             if val_count >= 7 and valid: 
                 valid_passports[counter] = temp
                 counter += 1
-
-
+                
     return valid_passports
 
 def check_valid_values(passports):
     count = 0
     for val in passports.values():
-        valid = False
+        temp_array = []
+        temp_count = 0
         for l in val:
             if l[0] == 'byr':
-                if len(l[1]) != 4: break
-                if (int(l[1]) < 1920 or int(l[1]) > 2002):
-                    break
-                print_valid(l[0], l[1])
+                if byr(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
             elif l[0] == 'cid': continue
             elif l[0] == 'iyr':
-                if len(l[1]) != 4: break
-                if (int(l[1]) < 2010 or int(l[1]) > 2020):
-                    break
-                print_valid(l[0], l[1])
+                if iyr(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
             elif l[0] == 'eyr':
-                if len(l[1]) != 4: break
-                if (int(l[1]) < 2020 or int(l[1]) > 2030):
-                    break
-                print_valid(l[0], l[1])
+                if eyr(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
             elif l[0] == 'hgt':
-                suffix = l[1][-2:]
-                if suffix != 'in' or suffix != 'cm': break
-                if suffix == 'in' and (int(l[1][:-2]) < 59 or int(l[1][:-2]) > 76): break
-                if suffix == 'cm' and (int(l[1][:-2]) < 150 or int(l[1][:-2]) > 193): break
-                print_valid(l[0], l[1])
+                if hgt(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
             elif l[0] == 'hcl':
-                if l[1][0] != '#': break
-                hcl_vals = {'0': True, '1': True, '2': True, '3': True, '4': True, '5': True, '6': True, '7': True, '8': True, '9': True, 'a': True, 'b': True, 'c': True, 'd': True, 'e': True, 'f': True}
-                if len(l[1][1:]) != 6: break
-                for element in l[1][1:]:
-                    if element not in hcl_vals: break
-                print_valid(l[0], l[1])
+                if hcl(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
             elif l[0] == 'ecl':
-                eye_colors = {'amb': True, 'blu': True, 'brn': True, 'gry': True, 'grn': True, 'hzl': True, 'oth': True}
-                if l[1] not in eye_colors: break
-                print_valid(l[0], l[1])
+                if ecl(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
             elif l[0] == 'pid':
-                if len(l[1]) != 9: break
-                for num in l[1]:
-                    if not num.isdigit(): break
-                print_valid(l[0], l[1])
-            valid = True
-        if valid: count += 1
+                if pid(l):
+                    temp_count += 1
+                    temp_array.append(l)
+                else: break
+            
+            if temp_count >= 7:
+                count += 1
+                break
 
     return count
 
-def print_valid(k, val):
-    print(k, val)
+def pid(pid):
+    if len(pid[1]) != 9: return False
+    for num in pid[1]:
+        if not num.isdigit(): return False
+    return True
 
+def eyr(eyr):
+    if len(eyr[1]) == 4 and (int(eyr[1]) >= 2020 and int(eyr[1]) <= 2030):        
+        return True
+    else: return False
+
+def byr(byr):
+    if len(byr[1]) == 4 and (int(byr[1]) >= 1920 and int(byr[1]) <= 2002):
+        return True
+    else:
+        return False
+
+def iyr(iyr):
+    if len(iyr[1]) == 4 and (int(iyr[1]) >= 2010 and int(iyr[1]) <= 2020):
+        return True
+    else:
+        return False
+
+def hgt(hgt):
+    suffix = hgt[1][-2:]
+    if suffix == 'in':
+        if int(hgt[1][:-2]) >= 59 and int(hgt[1][:-2]) <= 76: 
+            return True
+        else:
+            return False
+    elif suffix == 'cm':
+        if int(hgt[1][:-2]) >= 150 and int(hgt[1][:-2]) <= 193:
+            return True
+        else:
+            return False
+
+def hcl(hcl):
+    if hcl[1][0] != '#': return False
+    hcl_vals = {'0': True, '1': True, '2': True, '3': True, '4': True, '5': True, '6': True, '7': True, '8': True, '9': True, 'a': True, 'b': True, 'c': True, 'd': True, 'e': True, 'f': True}
+    if len(hcl[1][1:]) != 6: return False
+    hcl_count = 0
+    for element in hcl[1][1:]:
+        if element not in hcl_vals: return False
+        else: 
+            hcl_count += 1
+            continue
+    if hcl_count == 6:
+        return True
+    else:
+        return False
+
+def ecl(ecl):
+    eye_colors = {'amb': True, 'blu': True, 'brn': True, 'gry': True, 'grn': True, 'hzl': True, 'oth': True}
+    if ecl[1] not in eye_colors: return False
+    else:
+        return True
+
+# Print function for debugging
+# def print_valid(k, val):
+#     print(k, val)
 
 print(check_valid_values(clean_data()))
